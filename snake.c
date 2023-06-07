73d3f7a232041e28d76013414b1012c86f2f2c3c
Bhurtel Maheshwor <bhurtelace@gmail.com>
2023-06-07 17:07:33 +0900

    Mac Version Updated

--- snake.c
+++ snake.c
@@ -1,184 +1,145 @@
-#include <stdio.h>
-#include <conio.h>
+// Esc - quit
+// h - left
+// j - down
+// k - up
+// l - left
+
+
+
 #include <stdlib.h>
-#include <windows.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
 
-#define WIDTH 20
-#define HEIGHT 20
-#define MAX_LENGTH (WIDTH*HEIGHT)
+#define COLS 60
+#define ROWS 30
 
-// グローバル変数
-int headX, headY;
-int fruitX, fruitY;
-int score;
-int gameOver;
-int tailX[MAX_LENGTH], tailY[MAX_LENGTH];
-int tailLength;
-enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
-enum eDirection dir;
+int main() {
+  // Hide cursor
+  printf("\e[?25l");
 
-// 初期化関数
-void Initialize()
-{
-    gameOver = 0;
-    dir = STOP;
-    headX = WIDTH / 2;
-    headY = HEIGHT / 2;
-    fruitX = rand() % WIDTH;
-    fruitY = rand() % HEIGHT;
-    score = 0;
-}
+  // Switch to canonical mode, disable echo
+  struct termios oldt, newt;
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 
-// フレーム描画関数
-void DrawFrame()
-{
-    system("cls");
-    int i, j;
-    for (i = 0; i < WIDTH + 2; i++) {
-        printf("■");
+  int x[1000], y[1000];
+  int quit = 0;
+  while (!quit) {
+    // Render table
+    printf("┌");
+    for (int i = 0; i < COLS; i++)
+      printf("─");
+    printf("┐\n");
+
+    for (int j = 0; j < ROWS; j++) {
+      printf("│");
+      for (int i = 0; i < COLS; i++)
+        printf("·");
+      printf("│\n");
     }
-    printf("\n");
 
-    for (i = 0; i < HEIGHT; i++) {
-        for (j = 0; j < WIDTH; j++) {
-            if (j == 0) {
-                printf("■");
-            }
+    printf("└");
+    for (int i = 0; i < COLS; i++)
+      printf("─");
+    printf("┘\n");
 
-            if (i == headY && j == headX) {
-                printf("●"); // ヘビの頭部
-            }
-            else if (i == fruitY && j == fruitX) {
-                printf("◎"); // フルーツ
-            }
-            else {
-                int isTail = 0;
-                int k;
-                for (k = 0; k < tailLength; k++) {
-                    if (tailX[k] == j && tailY[k] == i) {
-                        printf("■"); // ヘビの体部
-                        isTail = 1;
-                    }
-                }
-                if (!isTail) {
-                    printf("  "); // 空白
-                }
-            }
+    // Move cursor back to top
+    printf("\e[%iA", ROWS + 2);
 
-            if (j == WIDTH - 1) {
-                printf("■");
-            }
+    int head = 0, tail = 0;
+    x[head] = COLS / 2;
+    y[head] = ROWS / 2;
+    int gameover = 0;
+    int xdir = 1, ydir = 0;
+    int applex = -1, appley;
+
+    while (!quit && !gameover) {
+      if (applex < 0) {
+        // Create new apple
+        applex = rand() % COLS;
+        appley = rand() % ROWS;
+
+        for (int i = tail; i != head; i = (i + 1) % 1000)
+          if (x[i] == applex && y[i] == appley)
+            applex = -1;
+
+        if (applex >= 0) {
+          // Draw apple
+          printf("\e[%iB\e[%iC❤", appley + 1, applex + 1);
+          printf("\e[%iF", appley + 1);
         }
-        printf("\n");
-    }
+      }
 
-    for (i = 0; i < WIDTH + 2; i++) {
-        printf("■");
-    }
-    printf("\n");
-    printf("Score: %d\n", score);
-}
+      // Clear snake tail
+      printf("\e[%iB\e[%iC·", y[tail] + 1, x[tail] + 1);
+      printf("\e[%iF", y[tail] + 1);
 
-// 入力受付関数
-void Input()
-{
-    if (_kbhit()) {
-        switch (_getch()) {
-        case 'a':
-            dir = LEFT;
-            break;
-        case 'd':
-            dir = RIGHT;
-            break;
-        case 'w':
-            dir = UP;
-            break;
-        case 's':
-            dir = DOWN;
-            break;
-        case 'x':
-            gameOver = 1;
-            break;
+      if (x[head] == applex && y[head] == appley) {
+        applex = -1;
+        printf("\a"); // Bell
+      } else
+        tail = (tail + 1) % 1000;
+
+      int newhead = (head + 1) % 1000;
+      x[newhead] = (x[head] + xdir + COLS) % COLS;
+      y[newhead] = (y[head] + ydir + ROWS) % ROWS;
+      head = newhead;
+
+      for (int i = tail; i != head; i = (i + 1) % 1000)
+        if (x[i] == x[head] && y[i] == y[head])
+          gameover = 1;
+
+      // Draw head
+      printf("\e[%iB\e[%iC▓", y[head] + 1, x[head] + 1);
+      printf("\e[%iF", y[head] + 1);
+      fflush(stdout);
+
+      usleep(5 * 1000000 / 60);
+
+      // Read keyboard
+      struct timeval tv;
+      fd_set fds;
+      tv.tv_sec = 0;
+      tv.tv_usec = 0;
+
+      FD_ZERO(&fds);
+      FD_SET(STDIN_FILENO, &fds);
+      select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv);
+      if (FD_ISSET(STDIN_FILENO, &fds)) {
+        int ch = getchar();
+        if (ch == 27 || ch == 'q') {
+          quit = 1;
+        } else if (ch == 'h' && xdir != 1) {
+          xdir = -1;
+          ydir = 0;
+        } else if (ch == 'l' && xdir != -1) {
+          xdir = 1;
+          ydir = 0;
+        } else if (ch == 'j' && ydir != -1) {
+          xdir = 0;
+          ydir = 1;
+        } else if (ch == 'k' && ydir != 1) {
+          xdir = 0;
+          ydir = -1;
         }
+      }
     }
+
+    if (!quit) {
+      // Show game over
+      printf("\e[%iB\e[%iC Game Over! ", ROWS / 2, COLS / 2 - 5);
+      printf("\e[%iF", ROWS / 2);
+      fflush(stdout);
+      getchar();
+    }
+  }
+
+  // Show cursor
+  printf("\e[?25h");
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  return 0;
 }
-
-// 更新関数
-void Update()
-{
-    int prevX = tailX[0];
-    int prevY = tailY[0];
-    int prev2X, prev2Y;
-    tailX[0] = headX;
-    tailY[0] = headY;
-    int i;
-    for (i = 1; i < tailLength; i++) {
-        prev2X = tailX[i];
-        prev2Y = tailY[i];
-        tailX[i] = prevX;
-        tailY[i] = prevY;
-        prevX = prev2X;
-        prevY = prev2Y;
-    }
-
-    switch (dir) {
-        case LEFT:
-        headX--;
-        break;
-    case RIGHT:
-        headX++;
-        break;
-    case UP:
-        headY--;
-        break;
-    case DOWN:
-        headY++;
-        break;
-    }
-
-    // ゲームオーバーの条件
-    if (headX < 0 || headX >= WIDTH || headY < 0 || headY >= HEIGHT) {
-        gameOver = 1;
-    }
-
-    for (i = 0; i < tailLength; i++) {
-        if (tailX[i] == headX && tailY[i] == headY) {
-            gameOver = 1;
-            break;
-        }
-    }
-
-    // フルーツを取った場合
-    if (headX == fruitX && headY == fruitY) {
-        score += 10;
-        fruitX = rand() % WIDTH;
-        fruitY = rand() % HEIGHT;
-        tailLength++;
-    }
-}
-
-int main()
-{
-    // ゲームの初期化
-    Initialize();
-
-    while (!gameOver) {
-        // フレームの描画
-        DrawFrame();
-
-        // 入力の受付
-        Input();
-
-        // ゲームの更新
-        Update();
-
-        // 速度調整
-        Sleep(10);
-    }
-
-    printf("ゲームオーバー\n");
-    printf("最終スコア: %d\n", score);
-
-    return 0;
-}
-
